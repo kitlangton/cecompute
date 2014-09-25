2e9198c0595c23bd58c03f64c5b9ca25a73374c8
Kit Langton <kitlangton@gmail.com>
2014-09-25 17:50:18 -0400

    Added Procfile

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ActiveRecord::Base
   has_many :order_services
   has_many :services, :through => :order_services
+
+  def total
+    total = 0 
+    self.services.each do |service|
+      total += service.price
+    end
+    total
+  end
 end
