b448effa3d0257d4b23f1b0e2c4a6972e7ef159d
Kit Langton <kitlangton@gmail.com>
2014-09-25 12:40:32 -0400

    Titleize Name on Creation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,12 @@
 class Service < ActiveRecord::Base
   validates :price, presence: true
+  validates :price, :numericality => {allow_blank: true, :less_than => 99999999, message: "a bit pricey, eh?"}
   validates :name, presence: true
 
+  before_save :titleize_name
+
+  def titleize_name
+    self.name = self.name.titleize
+  end
 
 end
