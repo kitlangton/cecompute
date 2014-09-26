02482336e15991d0326249e4de00d14977dc859f
Kit Langton <kitlangton@gmail.com>
2014-09-26 13:24:46 -0400

    Fix Validation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < ActiveRecord::Base
   validates :price, presence: true
   validates :price, :numericality => {allow_blank: true, :less_than => 99999999, message: "a bit pricey, eh?"}
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   has_many :order_services
   has_many :orders, :through => :order_services
