d813902f1adeccbf3f9628e1c3e616906feef397
Kit Langton <kitlangton@gmail.com>
2014-10-07 12:57:06 -0400

    Fix user cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   has_many :orders
   has_one :cart
+  before_save :create_cart
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def create_cart
+    Cart.create(user_id: self.id)
+  end
 end
